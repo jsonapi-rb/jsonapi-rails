2a37286390a4b1847a9a7a7006eefb2c68fcb500
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-11-14 21:35:03 +0100

    Fix deserialization. (#1)

--- lib/jsonapi/rails/action_controller.rb
+++ lib/jsonapi/rails/action_controller.rb
@@ -7,54 +7,63 @@ module JSONAPI
       def self.included(base)
         base.class_eval do
           extend ClassMethods
-          prepend InstanceMethods
         end
       end
 
       module ClassMethods
-        def deserializable_resource(key, klass = nil, &block)
+        def deserializable_resource(key, *args, &block)
+          klass = args.shift unless args.first.is_a?(Hash)
+          options = args.first || {}
           if klass.nil?
             klass = Class.new(JSONAPI::Deserializable::Resource, &block)
           end
-          @deserializable_key = key
-          @deserializable_class = klass
-          @deserializable_parser = JSONAPI::Parser::Resource
+          use DeserializeResource, key, klass, options
         end
 
-        def deserializable_relationship(key, klass = nil, &block)
+        def deserializable_relationship(key, *args, &block)
+          klass = args.shift unless args.first.is_a?(Hash)
+          options = args.first || {}
           if klass.nil?
             klass = Class.new(JSONAPI::Deserializable::Relationship, &block)
           end
-          @deserializable_key = key
-          @deserializable_class = klass
-          @deserializable_parser = JSONAPI::Parser::Relationship
-        end
-
-        def deserializable_class
-          @deserializable_class
-        end
-
-        def deserializable_key
-          @deserializable_key
-        end
-
-        def deserializable_parser
-          @deserializable_parser
+          use DeserializeResource, key, klass, options
         end
       end
 
-      module InstanceMethods
-        def params
-          params = super
-          key = self.class.deserializable_key
-          return params if body.key?(key)
-          parser = self.class.deserializable_parser
-          return params unless parser
+      class DeserializationMiddleware
+        JSONAPI_KEYS = %w(data meta links jsonapi).freeze
+        def initialize(app, key, klass)
+          @app = app
+          @deserializable_key = key
+          @deserializable_class = klass
+        end
 
+        def call(env)
+          request = Rack::Request.new(env)
+          body = request.params.slice(*JSONAPI_KEYS)
           parser.parse!(body)
-          deserializable_class = self.class.deserializable_class
+          deserialized_hash = @deserializable_class.call(body)
+          jsonapi = {}
+          JSONAPI_KEYS.each do |key|
+            next unless request.params.key?(key)
+            jsonapi[key.to_sym] = request.delete_param(key)
+          end
+          request.update_param(:_jsonapi, jsonapi)
+          request.update_param(@deserializable_key, deserialized_hash)
 
-          params.merge!(key => deserializable_class.call(params))
+          @app.call(env)
+        end
+      end
+
+      class DeserializeResource < DeserializationMiddleware
+        def parser
+          JSONAPI::Parser::Resource
+        end
+      end
+
+      class DeserializeRelationship < DeserializationMiddleware
+        def parser
+          JSONAPI::Parser::Relationship
         end
       end
     end
