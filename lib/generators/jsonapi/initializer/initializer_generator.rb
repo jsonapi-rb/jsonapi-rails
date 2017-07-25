a904687a74a6110048be41980d913f8f11d0c385
Lucas Hosseini <lucas.hosseini@gmail.com>
2017-07-25 13:18:22 +0200

    Add initializer generator. (#39)
    Add initializer generator.

---
+++ lib/generators/jsonapi/initializer/initializer_generator.rb
@@ -0,0 +1,7 @@
+class InitializerGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def copy_initializer_file
+    copy_file 'initializer.rb', 'config/initializers/jsonapi.rb'
+  end
+end
