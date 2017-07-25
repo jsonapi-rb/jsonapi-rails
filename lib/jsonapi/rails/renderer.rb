f8f84899feec16b07b00591ad4bfee82f767eed2
Lucas <lucas.hosseini@gmail.com>
2017-07-25 10:02:58 +0200

    Refactor renderers.

--- lib/jsonapi/rails/renderer.rb
+++ lib/jsonapi/rails/renderer.rb
@@ -9,7 +9,17 @@ module JSONAPI
         freeze
       end
 
-      def render(resources, options)
+      def render(resources, options, controller)
+        options = options.dup
+
+        if (pagination_links = controller.jsonapi_pagination(resources))
+          (options[:links] ||= {}).merge!(pagination_links)
+        end
+        options[:expose]  =
+          controller.jsonapi_expose.merge!(options[:expose] || {})
+        options[:jsonapi] =
+          options[:jsonapi_object] || controller.jsonapi_object
+
         @renderer.render(resources, options)
       end
     end
@@ -21,8 +31,10 @@ module JSONAPI
         freeze
       end
 
-      def render(errors, options)
+      def render(errors, options, controller)
+        options = options.merge(_jsonapi_pointers: controller.jsonapi_pointers)
         # TODO(beauby): SerializableError inference on AR validation errors.
+
         @renderer.render(errors, options)
       end
     end
