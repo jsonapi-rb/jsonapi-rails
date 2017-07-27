ce35773ed4f7a2398466007d4b608de97c959abd
Lucas <lucas.hosseini@gmail.com>
2017-07-27 14:57:52 +0200

    Add tests.

---
+++ spec/render_jsonapi_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe ActionController::Base, '#render', type: :controller do
+  context 'when calling render jsonapi: user' do
+    controller do
+      def index
+        serializer = Class.new(JSONAPI::Serializable::Resource) do
+          type :users
+          attribute :name
+        end
+        user = OpenStruct.new(id: 1, name: 'Lucas')
+
+        render jsonapi: user, class: serializer
+      end
+    end
+
+    subject { JSON.parse(response.body) }
+    let(:serialized_user) do
+      {
+        'data' => {
+          'id' => '1',
+          'type' => 'users',
+          'attributes' => { 'name' => 'Lucas' }
+        }
+      }
+    end
+
+    it 'renders a JSON API success document' do
+      get :index
+
+      expect(response.content_type).to eq('application/vnd.api+json')
+      is_expected.to eq(serialized_user)
+    end
+  end
+
+  context 'when specifying a default jsonapi object' do
+    controller do
+      def index
+        render jsonapi: nil
+      end
+
+      def jsonapi_object
+        { version: '1.0' }
+      end
+    end
+
+    subject { JSON.parse(response.body) }
+    let(:document) do
+      {
+        'data' => nil,
+        'jsonapi' => { 'version' => '1.0' }
+      }
+    end
+
+    it 'renders a JSON API success document' do
+      get :index
+
+      expect(response.content_type).to eq('application/vnd.api+json')
+      is_expected.to eq(document)
+    end
+  end
+end
