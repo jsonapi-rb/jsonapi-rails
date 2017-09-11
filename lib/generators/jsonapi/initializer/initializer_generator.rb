2597d0f462f92a2a520407e15c7a9a9a7198ed71
Sergei Maximov <s.b.maximov@gmail.com>
2017-09-11 20:10:33 +0300

    Fix module namespace for initializer generator (#48)

--- lib/generators/jsonapi/initializer/initializer_generator.rb
+++ lib/generators/jsonapi/initializer/initializer_generator.rb
@@ -1,7 +1,9 @@
-class InitializerGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+module Jsonapi
+  class InitializerGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-  def copy_initializer_file
-    copy_file 'initializer.rb', 'config/initializers/jsonapi.rb'
+    def copy_initializer_file
+      copy_file 'initializer.rb', 'config/initializers/jsonapi.rb'
+    end
   end
 end
