24161d2da4d8bc35b8a0caf3d226778fd3fcca67
Lucas <lucas.hosseini@gmail.com>
2017-09-12 11:29:11 +0200

    Wire fields/include hooks.

--- lib/jsonapi/rails/renderer.rb
+++ lib/jsonapi/rails/renderer.rb
@@ -18,17 +18,21 @@ module JSONAPI
 
       private
 
+      # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
       def default_options(options, controller, resources)
         options.dup.tap do |opts|
           opts[:class] ||= controller.jsonapi_class
           if (pagination_links = controller.jsonapi_pagination(resources))
             opts[:links] = (opts[:links] || {}).merge(pagination_links)
           end
-          opts[:expose]  = controller.jsonapi_expose.merge(opts[:expose] || {})
+          opts[:expose] = controller.jsonapi_expose.merge(opts[:expose] || {})
+          opts[:fields] ||= controller.jsonapi_fields
+          opts[:include] ||= controller.jsonapi_include
           opts[:jsonapi] = opts.delete(:jsonapi_object) ||
                            controller.jsonapi_object
         end
       end
+      # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
     end
 
     # @private
