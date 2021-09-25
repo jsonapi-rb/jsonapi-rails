0cc64d03b898ec5df5a68e8e09e2564dc083c7cc
Malcolm Baig <git@baig.org>
2021-09-25 14:50:54 +0100

    Add render jsonapi specs that cover caching
    These specs test the caching version of the render call more thoroughly.
    
    - Assert that the expected data structure is stored in the cache
    - Assert that the response is the same whether using the caching version
      of this call or the regular one

--- spec/render_jsonapi_spec.rb
+++ spec/render_jsonapi_spec.rb
@@ -23,32 +23,67 @@ describe ActionController::Base, '#render', type: :controller do
       def serializer
         Class.new(JSONAPI::Serializable::Resource) do
           type 'users'
-          attribute :name
+          attributes :id, :name, :dob
 
           def jsonapi_cache_key(*)
-            'foo'
+            'cache_key'
           end
         end
       end
 
-      def index
-        user = OpenStruct.new(id: 1, name: 'Lucas')
+      def user
+        OpenStruct.new(id: 1, name: 'Johnny Cache', dob: Time.new(2021,1,1))
+      end
 
-        render jsonapi: user,
+      def index
+        render jsonapi: [user],
+               class: { OpenStruct: serializer }
+      end
+
+      def index_with_caching
+        render jsonapi: [user],
                class: { OpenStruct: serializer },
                cache: Rails.cache
       end
     end
 
-    subject { JSON.parse(response.body) }
+    before do
+      routes.draw do
+        get "index_with_caching" => "anonymous#index_with_caching"
+        get "index" => "anonymous#index"
+      end
+    end
+
+    let(:rendered_json) { JSON.parse(response.body) }
 
     it 'renders a JSON API success document' do
-      get :index
-      expect(Rails.cache.exist?('foo')).to be true
-      get :index
+      get :index_with_caching
 
       expect(response.content_type).to eq('application/vnd.api+json')
-      expect(subject.key?('data')).to be true
+      expect(rendered_json.key?('data')).to be true
+    end
+
+    it 'caches resources' do
+      get :index_with_caching
+
+      expect(Rails.cache.exist?('cache_key')).to be true
+      expect(JSON.parse(Rails.cache.read('cache_key'))).to eq rendered_json['data'].first
+    end
+
+    it 'renders equivalent JSON whether caching or not' do
+      expected_response = {
+        "data"=>[{"id"=>"1", "type"=>"users", "attributes"=>{"id"=>1, "name"=>"Johnny Cache", "dob"=>"2021-01-01T00:00:00.000+00:00"}}],
+        "jsonapi"=>{"version"=>"1.0"}
+      }
+
+      get :index
+      response_with_no_caching = rendered_json.deep_dup
+
+      get :index_with_caching
+      response_with_caching = rendered_json
+
+      expect(response_with_no_caching).to eq expected_response
+      expect(response_with_caching).to eq expected_response
     end
   end
 end
